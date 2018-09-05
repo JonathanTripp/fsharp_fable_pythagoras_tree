de1f881a94f499d89acd1bfc09e40a6b32313a0b
Jonathan Tripp <jonathan@trippsoft.co.uk>
2018-09-05 23:24:05 +0100

    Add angle range

--- src/App.fs
+++ src/App.fs
@@ -3,6 +3,7 @@
 
 module fsharp_fable_pythagorus_tree
 
+open System
 open Fable.Core
 open Fable.Core.JsInterop
 open Fable.Import
@@ -16,7 +17,7 @@ type Line = { left : Point; right : Point }
 let svg_square x1 y1 x2 y2 x3 y3 x4 y4 =
     (s=?"polygon") [ "points" => sprintf "%i %i %i %i %i %i %i %i" (int x1) (int y1) (int x2) (int y2) (int x3) (int y3) (int x4) (int y4) ] []
 
-let sprout line =
+let sprout line theta =
     let dx = line.right.x - line.left.x
     let dy = line.left.y - line.right.y
     let line2 = {
@@ -24,8 +25,8 @@ let sprout line =
         right = { x = line.right.x - dy ; y = line.right.y - dx }
     }
     let triangleTop = {
-        x = line2.left.x + (dx - dy) / 2.;
-        y = line2.left.y - (dx + dy) / 2.
+        x = line2.left.x + dx * Math.Cos(theta) * Math.Cos(theta) - dy * Math.Sin(theta) * Math.Cos(theta);
+        y = line2.left.y - dy * Math.Cos(theta) * Math.Cos(theta) - dx * Math.Sin(theta) * Math.Cos(theta)
     }
     [
         { left = line2.left; right = triangleTop }
@@ -38,30 +39,33 @@ let draw_square line =
     svg_square line.left.x line.left.y line.right.x line.right.y
                (line.right.x - dy) (line.right.y - dx) (line.left.x - dy) (line.left.y - dx)
  
-let rec generate lines = function
+let rec generate lines theta = function
 | 0 -> lines
 | n ->
     let next =
         lines
         |> List.collect (fun line ->
-            sprout line
+            sprout line theta
         )
     
     let all = List.append lines next
-    generate all (n-1)
+    generate all theta (n-1)
 
 let renderLines lines =
     (s=?"svg") ["width" => 600; "height" => 600] (List.map draw_square lines)
 
 let iters = document.getElementById("iterations") :?> HTMLInputElement
+let angle = document.getElementById("angle") :?> HTMLInputElement
 
 let init() =
     let output = document.getElementById("output")
 
     let depth = (int iters.value)
-    let lines = generate [{ left = { x = 275.; y = 500. }; right = { x = 375.; y = 500. } }] depth
+    let theta = (float angle.value) * Math.PI / 180.0
+    let lines = generate [{ left = { x = 275.; y = 500. }; right = { x = 375.; y = 500. } }] theta depth
     renderLines lines |> renderTo output
 
 iters.addEventListener_input(fun _ -> init(); box())
+angle.addEventListener_input(fun _ -> init(); box())
 
 init()
